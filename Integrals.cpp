82f776d08f817fda481985de2c5de1bebf8944dd
stasiiazh <nastiazhlobich@icloud.com>
2023-12-13 01:23:45 +0300

    исправлена лаба 5, добавлен график для лабы 4

--- Integrals.cpp
+++ Integrals.cpp
@@ -9,49 +9,54 @@ typedef double (*FuncPtr)(double);
 // Тип для указателя на функцию двух переменных
 typedef double (*Func2Ptr)(double, double);
 
-double trapezoidal(FuncPtr f, double a, double b, int n) {
-    double h = (b - a) / n;
-    double sum = 0.5 * (f(a) + f(b));
-    for (int i = 1; i < n; i++) {
-        sum += f(a + i * h);
-    }
-    return h * sum;
+double trapezoidal(FuncPtr f, double a, double b, int n, double eps) {
+    double integral_n, integral_2n, h_n, h_2n, sum_n, sum_2n;
+
+    do {
+        h_n = (b - a) / n;
+        sum_n = 0.5 * (f(a) + f(b));
+        for (int i = 1; i < n; i++) {
+            sum_n += f(a + i * h_n);
+        }
+        integral_n = h_n * sum_n;
+
+        h_2n = (b - a) / (2 * n);
+        sum_2n = 0.5 * (f(a) + f(b));
+        for (int i = 1; i < 2 * n; i++) {
+            sum_2n += f(a + i * h_2n);
+        }
+        integral_2n = h_2n * sum_2n;
+
+        if (fabs(integral_n - integral_2n) >= 3 * eps) {
+            n *= 2;
+        }
+    } while (fabs(integral_n - integral_2n) >= 3 * eps);
+
+    return integral_2n; // Возвращаем значение
 }
 
 double simpsons(FuncPtr f, double a, double b, int n, double eps) {
-    double integral_n, integral_2n, h_n, h_2n, sum_n, sum_2n;
-    integral_n = integral_2n = 0.0;
+    double integral, integral_prev;
+    integral = integral_prev = 0.0;
 
     do {
-        n = (n % 2 == 0) ? n : n + 1; 
-        h_n = (b - a) / n;
-        h_2n = (b - a) / (2 * n); 
-        sum_n = sum_2n = 0.0;
+        integral_prev = integral;
+        n = (n % 2 == 0) ? n : n + 1; // Убедимся, что n чётное
+        double h = (b - a) / n;
+        double sum = f(a) + f(b);
 
-        sum_n = f(a) + f(b);
         for (int i = 1; i < n; i += 2) {
-            sum_n += 4 * f(a + i * h_n);
+            sum += 4 * f(a + i * h);
         }
         for (int i = 2; i < n; i += 2) {
-            sum_n += 2 * f(a + i * h_n);
+            sum += 2 * f(a + i * h);
         }
-        integral_n = h_n / 3 * sum_n;
 
-        sum_2n = f(a) + f(b);
-        for (int i = 1; i < 2 * n; i += 2) {
-            sum_2n += 4 * f(a + i * h_2n);
-        }
-        for (int i = 2; i < 2 * n; i += 2) {
-            sum_2n += 2 * f(a + i * h_2n);
-        }
-        integral_2n = h_2n / 3 * sum_2n;
+        integral = h / 3 * sum;
+        n *= 2;
+    } while (fabs(integral - integral_prev) > 15 * eps);
 
-        if (fabs(integral_n - integral_2n) > (n == 2 ? 3 : 15) * eps) {
-            n *= 2;
-        }
-    } while (fabs(integral_n - integral_2n) > (n == 2 ? 3 : 15) * eps);
-
-    return integral_2n; 
+    return integral_prev;
 }
 
 double simpsons2(Func2Ptr f, double ax, double bx, int nx, double ay, double by, int ny) {
@@ -63,8 +68,8 @@ double simpsons2(Func2Ptr f, double ax, double bx, int nx, double ay, double by,
         for (int j = 0; j <= ny; j++) {
             double x = ax + i * hx;
             double y = ay + j * hy;
-            double weight = (i == 0 || i == nx) ? 1 : (i % 2 == 1) ? 4 : 2;
-            weight *= (j == 0 || j == ny) ? 1 : (j % 2 == 1) ? 4 : 2;
+            double weight = (i == 0 or i == nx) ? 1 : (i % 2 == 1) ? 4 : 2;
+            weight *= (j == 0 or j == ny) ? 1 : (j % 2 == 1) ? 4 : 2;
             sum += weight * f(x, y);
         }
     }
@@ -86,7 +91,7 @@ int main() {
     int n = 100;
     double eps = 0.0001; 
 
-    double integralTrapezoidal = trapezoidal(func, a, b, n);
+    double integralTrapezoidal = trapezoidal(func, a, b, n, eps);
     cout << "Integral (Trapezoidal): " << integralTrapezoidal << endl;
 
     double integralSimpson = simpsons(func, a, b, n, eps);
@@ -100,3 +105,4 @@ int main() {
 
     return 0;
 }
+
